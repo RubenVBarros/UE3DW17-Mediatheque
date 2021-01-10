37bb6b557c85ecfd2290eb15061912ce3839ea4b
Romain <romain.vidon@laposte.net>
2021-01-10 16:24:52 +0100

    main: template de la médiathèque

--- routes/mediatheque.js
+++ routes/mediatheque.js
@@ -1,4 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function(req, res) {
+    var db = req.db;
+    var collection = db.get('documents');
+    collection.find({},{},function(e,docs){
+        res.render('mediatheque', {
+            "documents" : docs
+        });
+    });
+});
+
 module.exports = router;
