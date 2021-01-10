f06be93bc934631e992ef38374205daee4d0d9a9
Romain <romain.vidon@laposte.net>
2021-01-10 17:22:22 +0100

    main: route emprunter/rendre

--- routes/mediatheque.js
+++ routes/mediatheque.js
@@ -11,4 +11,30 @@ router.get('/', function(req, res) {
     });
 });
 
+router.patch('/:id/:action', function(req,res){
+    let id = req.params.id;
+    let action = req.params.action;
+    let db = req.db;
+    switch(action){
+        case "emprunter":
+            docs = db.get("documents");
+            docs.update({"_id":id},{"$set":{"emprunt":true}},function(error, result){
+                if(error){
+                    console.error(error.message);
+                }
+                res.send('ok'); 
+
+            });
+            break;
+        case "rendre":
+            docs = db.get("documents");
+            docs.update({"_id":id},{"$unset":{"emprunt":""}},function(){
+                res.send('ok');
+            });
+            break;
+        default:
+            res.send("Action incorrecte.");
+    }
+});
+
 module.exports = router;
